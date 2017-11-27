534f5bca4f37d5b5d19cce8693ac7f3a42cf30ed
chenjianwei <chenjianwei@ddmh5.com>
2017-11-27 15:30:18 +0800

    收藏功能准备开发

---
+++ src/routes/normal/MySaveAgent.js
@@ -0,0 +1,222 @@
+import React from 'react';
+import { connect } from 'dva';
+
+import { NavBar, ListViewTable } from '@/helps/antdComponent';
+import { Title, SearchBar } from '@/helps/styleComponent';
+import BaseComponent from '@/helps/BaseComponent';
+// import { FlexRow, Flex, BaseFont } from '../utils/styleComponent';
+import styles from './MyUnderAgent.css';
+
+class MySaveAgent extends BaseComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: [],
+      isLoaded: false,
+      searchVal: '',
+    };
+  }
+  rechargeForAgent = (agentId) => {
+    this.props.dispatch(this.helps.routerRedux.push({
+      pathname: 'rechargeForAgent',
+      query: {
+        agentId,
+      },
+    }));
+  }
+  async componentWillMount() {
+    const res = await this.helps.webHttp.get('/spreadApi/mySaveAgent');
+    if (res.isSuccess) {
+      this.setState({
+        tableData: res.data,
+      });
+    }
+  }
+  // onEndReached = () => {
+  //   console.log('滚动到某个位置');
+  // }
+  // 添加收藏
+  navigateToSaveAgent = () => {
+    this.props.dispatch(this.helps.routerRedux.push('/saveAgent'));
+  }
+  onSearchInputChange = (ev) => {
+    this.setState({
+      searchVal: ev.target.value,
+    });
+  }
+  onCancelClick = () => {
+    this.setState({
+      searchVal: '',
+    });
+  }
+  renderHeader = (columnsData) => { // dataIndex title
+    return (
+      <div className={styles.rowSection}>
+        {
+          columnsData.map(({ title, dataIndex, remark, i }) => (
+            <div
+              key={dataIndex + i}
+              style={{
+                width: `${100 / columnsData.length}%`,
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                color: '#000',
+              }}
+            >
+              <p>{ title }</p>
+              <p>{ remark }</p>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+  powerArr = () => {
+    const self = this;
+    const columns = [
+      {
+        dataIndex: 'agentInviteCode',
+        title: '代理ID',
+      },
+      {
+        dataIndex: 'CommissionOfAll',
+        title: '总提成钻石数',
+        render(rowVal) {
+          if (rowVal.CommissionOfAll >= 0) {
+            return <div className="countAdd">{`+${rowVal.CommissionOfAll}`}</div>;
+          }
+          return <div className="countSub">{`-${rowVal.CommissionOfAll}`}</div>;
+        },
+      },
+      {
+        dataIndex: 'CommissionOfToday',
+        title: '今日提成钻石数',
+        render(rowVal) {
+          if (rowVal.CommissionOfToday >= 0) {
+            return <div className="countAdd">{`+${rowVal.CommissionOfToday}`}</div>;
+          }
+          return <div className="countSub">{`-${rowVal.CommissionOfToday}`}</div>;
+        },
+      },
+    ];
+    const { powerEnum } = this.helps;
+    const { powerList } = this.props;
+    const hasPowerToRecharge = powerList && powerList.findIndex((power) => {
+      return power === powerEnum.iAgentGiveForAnyAgent || power === powerEnum.iAgentGiveForAgent;
+    }) > -1;
+    if (hasPowerToRecharge) {
+      columns.push({
+        title: '操作',
+        render: (data) => {
+          return (<div
+            className={styles.rechargeBtn}
+            onClick={() => self.rechargeForAgent(data.agentInviteCode)}
+          >
+          充值
+          </div>);
+        },
+      });
+    }
+    return columns;
+  }
+  renderRowData = () => {
+    return (
+      <div style={{ margin: '0.5rem auto', textAlign: 'center' }}>
+        您还没有代理哦~赶紧去<span onClick={this.navigateToSaveAgent} className="color_base">添加</span>
+      </div>
+    );
+  }
+  render() {
+    const { tableData, searchVal } = this.state;
+    const filterTableData = tableData.filter((data) => {
+      return data.agentInviteCode.toString().indexOf(searchVal) !== -1;
+    });
+    // const allRechargeCount = tableData.reduce((beforeVal, currentVal) => {
+    //   return beforeVal + currentVal.allRechargeCount;
+    // }, 0);
+    // const allRechargeCountRemark = this.parseFloatMoney(allRechargeCount);// 总充值
+
+    // const rechargeCountOfToday = tableData.reduce((beforeVal, currentVal) => {
+    //   return beforeVal + currentVal.rechargeCountOfToday;
+    // }, 0);
+    // const rechargeCountOfTodayFloat = parseFloat(rechargeCountOfToday / 100).toFixed(2); // 今日充值
+
+    const CommissionOfAll = tableData.reduce((beforeVal, currentVal) => {
+      return beforeVal + currentVal.CommissionOfAll;
+    }, 0);
+    const CommissionOfToday = tableData.reduce((beforeVal, currentVal) => {
+      return beforeVal + currentVal.CommissionOfToday;
+    }, 0);
+    // 备注
+    const agentNumberRemark = `(共${tableData.length}人)`; // 我的下级代理人数
+    const CommissionOfAllRemark = `(共${CommissionOfAll}个)`; // 总提成
+
+    const CommissionOfTodayRemark = `共(${CommissionOfToday}个)`; // 今日提成
+    const columnsRemark = [agentNumberRemark, CommissionOfAllRemark, CommissionOfTodayRemark];
+    const columns = this.powerArr();
+    const columnsAddRemark = columns.map((item, i) => ({
+      ...item,
+      remark: columnsRemark[i],
+    }));
+
+    return (
+      <div className={styles.container}>
+        <Title>我收藏的代理</Title>
+        <NavBar
+          title="我收藏的代理"
+          onClick={() => this.props.dispatch(this.helps.routerRedux.goBack())}
+          right={<span onClick={this.navigateToSaveAgent}>添加</span>}
+        />
+        <SearchBar
+          placeholder="请输入代理ID"
+          maxLength={8}
+          onChange={this.onSearchInputChange}
+          value={searchVal}
+          onCancelClick={this.onCancelClick}
+        />
+        {
+          this.renderHeader(columns)
+        }
+        <ListViewTable
+          tableData={filterTableData}
+          columns={columnsAddRemark}
+          ListEmptyComponent={this.renderRowData()}
+        />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    ...state.agent,
+  };
+}
+
+export default connect(mapStateToProps)(MySaveAgent);
+
+
+/*
+ renderHeader={() => <span>header</span>}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? 'Loading...' : 'Loaded'}
+          </div>)}
+          renderSectionHeader={sectionData => (
+            <div>{`Task ${sectionData.split(' ')[1]}`}</div>
+          )}
+          className="fortest"
+          style={{
+            height: document.documentElement.clientHeight * 3 / 4,
+            overflow: 'auto',
+            border: '1px solid #ddd',
+            margin: '0.1rem 0',
+          }}
+          pageSize={4}
+          onScroll={() => { console.log('scroll'); }}
+          scrollRenderAheadDistance={500}
+          scrollEventThrottle={200}
+          onEndReached={this.onEndReached}
+          onEndReachedThreshold={10}
+*/
---
+++ src/routes/normal/MySavePlayer.js
@@ -0,0 +1,255 @@
+import React from 'react';
+import { connect } from 'dva';
+
+import { NavBar, ListViewTable, Icon } from '@/helps/antdComponent';
+import { Title, SearchBar } from '@/helps/styleComponent';
+import BaseComponent from '@/helps/BaseComponent';
+// import { FlexRow, Flex, BaseFont } from '../utils/styleComponent';
+import styles from './MyPlayer.css';
+
+class MySavePlayer extends BaseComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      tableData: [],
+      searchVal: '',
+      isSort: false, // 是否进行排序
+    };
+    this.serchInput = null; // 搜索的内容
+    this.serverid = null; // 游戏id
+    const self = this;
+
+    // 是否是正序排序
+    this.sortState = {
+      masonrySurplus: {
+        isSortUp: false, // 是否正序排序
+        isSort: false, // 是否在排序
+      },
+      recentlyLoginTime: {
+        isSortUp: false, // 是否正序排序
+        isSort: false, // 是否在排序
+      },
+    };
+    // this.forceUpdate();
+    
+    this.columns = [
+      {
+        dataIndex: 'playerId',
+        title: '玩家',
+        render: (data) => {
+          return (<div>
+            <p>{data.playerName}</p>
+            <p style={{ color: '#999' }}>ID:{data.playerId}</p>
+          </div>);
+        },
+      },
+      {
+        dataIndex: 'masonrySurplus',
+        title: '账户钻石',
+        render(rowVal) {
+          return <div className="countNor">{rowVal.masonrySurplus}</div>;
+        },
+      },
+      {
+        dataIndex: 'recentlyLoginTime',
+        title: '最近登录',
+        render(rowVal) {
+          const transRowTieme = new Date(rowVal.recentlyLoginTime).format('MM-dd hh:mm');
+          return <div>{transRowTieme}</div>;
+        },
+      },
+      {
+        title: '操作',
+        render: (data) => {
+          return (<div
+            className={styles.rechargeBtn}
+            onClick={() => self.payForMyPlayer(data.playerId)}
+          >
+          充值
+          </div>);
+        },
+      },
+    ];
+  }
+  // 切换排序
+  toggleSort = (sortType) => {
+    if (!this.state.isSort) {
+      this.sortState[sortType].isSort = true;
+      this.setState({
+        isSort: true,
+      });
+      return false;
+    }
+    for (const attr in this.sortState) {
+      if (this.sortState[attr]) {
+        // const element = object[key];
+        if (attr === sortType) {
+          if (this.sortState[attr].isSort) {
+            this.sortState[attr].isSortUp = !this.sortState[attr].isSortUp;
+          }
+          this.sortState[attr].isSort = true;
+        } else {
+          this.sortState[attr].isSort = false;
+        }
+      }
+    }
+    this.forceUpdate();
+  }
+  async componentWillMount() {
+    const { serverid } = this.helps.querystring.parse(this.props.location.search.substring(1));
+    this.serverid = serverid;
+    let res;
+    if (serverid) {
+      res = await this.helps.webHttp.get('/spreadApi/mySavePlayer', { serverid });
+    } else {
+      res = await this.helps.webHttp.get('/spreadApi/mySavePlayer');
+    }
+    if (res.isSuccess) {
+      this.setState({
+        tableData: res.data,
+      });
+    }
+  }
+  payForMyPlayer = (playerId) => {
+    this.props.dispatch(this.helps.routerRedux.push({ pathname: '/pay', query: { playerId, serverid: this.serverid || '' } }));
+  }
+  onSearchInputChange = (ev) => {
+    this.setState({
+      searchVal: ev.target.value,
+    });
+  }
+  onCancelClick = () => {
+    this.setState({
+      searchVal: '',
+    });
+  }
+  // 添加收藏的玩家
+  navigateToSavePlayer = () => {
+    this.props.dispatch(this.helps.routerRedux.push({ pathname: '/savePlayer', query: { serverid: this.serverid || '' } }));
+  }
+  renderRowData = () => {
+    return (
+      <div style={{ margin: '0.5rem auto', textAlign: 'center' }}>
+        您还没有玩家哦~赶紧去<span onClick={this.navigateToSavePlayer} className="color_base">添加</span>
+      </div>
+    );
+  }
+  renderHeader = (columnsData) => { // dataIndex title
+    const { isSort } = this.state;
+    return (
+      <div className={styles.rowSection}>
+        {
+          columnsData.map(({ title, dataIndex, remark, i }) => {
+            const sortInfo = this.sortState[dataIndex];
+            let sortIcon = null;
+            // console.log(sortInfo);
+            if (isSort && sortInfo && sortInfo.isSort) {
+              // console.log(sortInfo)
+              if (sortInfo.isSortUp) {
+                sortIcon = <Icon type="up" size="xs" />;
+              } else {
+                sortIcon = <Icon type="down" size="xs" />;
+              }
+            }
+            return (
+              <div
+                key={dataIndex + i}
+                style={{
+                  width: `${100 / columnsData.length}%`,
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  color: '#000',
+                }}
+              >
+                <p className={styles.tableTilteWrap} onClick={() => this.toggleSort(dataIndex)}>
+                  <span>{ title }</span>{sortIcon}
+                </p>
+                <p>{ remark }</p>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+  render() {
+    const { searchVal, tableData, isSort } = this.state;
+    const columns = this.columns;
+    let sortTableData = tableData;
+    if (isSort) {
+      for (const attr in this.sortState) {
+        if (this.sortState[attr] && this.sortState[attr].isSort) {
+          if (this.sortState[attr].isSortUp) {
+            sortTableData = sortTableData.sort((item1, item2) => {
+              return item1[attr] - item2[attr];
+            });
+          } else {
+            sortTableData = sortTableData.sort((item1, item2) => {
+              return item2[attr] - item1[attr];
+            });
+          }
+        }
+      }
+      
+      // const sortType = sortTypeArr[0];
+    }
+    const filterTableData = sortTableData.filter((data) => {
+      if (!searchVal) {
+        return true;
+      }
+      return data.playerId.toString().indexOf(searchVal) !== -1 || data.playerName.indexOf(searchVal) !== -1;
+    });
+    // const PalyCashAllCount = tableData.reduce((beforeVal, currentVal) => {
+    //   return beforeVal + currentVal.palyCashCount;
+    // }, 0);
+    // const masonrySurplusAllCount = tableData.reduce((beforeVal, currentVal) => {
+    //   return beforeVal + currentVal.masonrySurplus;
+    // }, 0);
+    // const transPalyCashCount = this.parseFloatMoney(PalyCashAllCount);
+    // const columnsRemark = [`(共${tableData.length}人)`, `(共${masonrySurplusAllCount}个)`, `(共${transPalyCashCount}元)`];
+    // const columnsAddRemark = columns.map((item, i) => ({
+    //   ...item,
+    //   remark: columnsRemark[i],
+    // }));
+    // const columnsAddRemark = columns.map(item => ({
+    //   ...item,
+    //   title: 'sdds'
+    // }));
+    return (
+      <div className={styles.container}>
+        <Title>我收藏的玩家</Title>
+        <NavBar
+          title="我收藏的玩家"
+          onClick={() => this.props.dispatch(this.helps.routerRedux.goBack())}
+          right={<div onClick={this.navigateToSavePlayer}>添加</div>}
+        />
+        <SearchBar
+          placeholder="输入玩家的ID/名称"
+          onChange={this.onSearchInputChange}
+          value={searchVal}
+          onCancelClick={this.onCancelClick}
+        />
+        {
+          this.renderHeader(columns)
+        }
+        <ListViewTable
+          tableData={filterTableData}
+          columns={columns}
+          sort={this.state.isSort}
+          ListEmptyComponent={this.renderRowData()}
+        />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    ...state.agent,
+  };
+}
+
+export default connect(mapStateToProps)(MySavePlayer);
